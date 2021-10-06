36042dac03deec58a64c02dced404dfb072019ec
Jordan Victor Scher <jordanvscher@hotmail.com>
2021-10-06 19:21:53 -0300

    refactor: move mapping to mongo helper

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -8,8 +8,6 @@ export class AccountMongoRepository implements AddAccountRepository {
     const accountCollection = MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne(accountData)
     const account = await accountCollection.findOne({ _id: result.insertedId })
-    const { _id, ...accountWithoutId } = account
-    // @ts-expect-error
-    return Object.assign({}, accountWithoutId, { id: _id })
+    return MongoHelper.map(account)
   }
 }
